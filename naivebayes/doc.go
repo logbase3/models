afbe53536fe633b510a478602f25186dd9a7fb2d
Roberto Lapuente <topo@asustin.net>
2014-04-09 02:38:15 -0500

    Add package description to godoc

--- naivebayes/doc.go
+++ naivebayes/doc.go
@@ -1,2 +1,27 @@
-//Package naivebayes
+/*
+	filequery is a small and simple tool for querying files using SQL.
+	Copyright (C) 2013 log₃() <contact@logbase3.com>
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+	For more information visit https://github.com/logbase3/fuery
+	or send an e-mail to contact@logbase3.com
+*/
+
+/*Package naivebayes implements a simple probabilistic classifier known as Naive Bayes.
+
+This classifier is based on the Bayes Theorem and it's call Naive since it makes strong
+independent assumptions.
+*/
 package naivebayes
